c09ed8d22beb67cb827744dc55c8f2503917f4ee
Lunarixus <Nathan@lunarixus.party>
2020-12-07 21:59:52 +0100

    taimen: Don't build VNDK on vendor
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -25,8 +25,6 @@ $(call inherit-product, vendor/google/taimen/taimen-vendor.mk)
 PRODUCT_COPY_FILES += \
     device/google/taimen/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
-PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
-
 PRODUCT_MANUFACTURER := Google
 PRODUCT_BRAND := Android
 PRODUCT_NAME := aosp_taimen
