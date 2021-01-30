3b2a4a656fd416bd8ebf37fa2ddfecc330bbf406
Lunarixus <Nathan@lunarixus.dev>
2021-01-30 19:27:34 +0000

    taimen: Move touchscreen config to a separate directory
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

--- device.mk
+++ device.mk
@@ -91,7 +91,7 @@ PRODUCT_COPY_FILES += \
 
 # touchscreen configuration
 PRODUCT_COPY_FILES += \
-    device/google/taimen/touchscreen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/touchscreen.idc
+    device/google/taimen/configs/keylayout/touchscreen.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/touchscreen.idc
 
 # Keymaster configuration
 PRODUCT_COPY_FILES += \
